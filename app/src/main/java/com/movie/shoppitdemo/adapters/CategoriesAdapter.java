5f5eec60868be9a9bc7cab7c562201a09a003ae2
Mohammed Rahman <minhaz357@gmail.com>
2022-04-13 16:28:18 -0400

    Change layout file names

--- app/src/main/java/com/movie/shoppitdemo/adapters/CategoriesAdapter.java
+++ app/src/main/java/com/movie/shoppitdemo/adapters/CategoriesAdapter.java
@@ -35,7 +35,7 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_row_category, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.row_category, parent, false);
         return new ViewHolder(view);
     }
 
---
+++ app/src/main/java/com/movie/shoppitdemo/adapters/CategoryDetailsAdapter.java
@@ -0,0 +1,6 @@
+package com.movie.shoppitdemo.adapters;
+
+public class CategoryDetailsAdapter {
+
+
+}
