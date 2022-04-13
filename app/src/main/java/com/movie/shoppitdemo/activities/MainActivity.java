74c243f424ca811529b4a1712c853f7931b3f14f
Mohammed Rahman <minhaz357@gmail.com>
2022-04-12 23:23:12 -0400

    Add bottom navigatio bar

---
+++ app/src/main/java/com/movie/shoppitdemo/activities/MainActivity.java
@@ -0,0 +1,97 @@
+package com.movie.shoppitdemo.activities;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.movie.shoppitdemo.R;
+import com.movie.shoppitdemo.fragments.CategoryFragment;
+import com.movie.shoppitdemo.fragments.HomeFragment;
+import com.movie.shoppitdemo.fragments.ProfileFragment;
+import com.movie.shoppitdemo.fragments.SearchFragment;
+import com.movie.shoppitdemo.fragments.ShopFragment;
+import com.movie.shoppitdemo.models.Item;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the elements
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // set click listener on bottom navigation
+        bottomNavigationView.setOnItemSelectedListener(navItemSelectedListener);
+
+        // Set default selection, in this case home item
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+        // get items
+        queryItem();
+    }
+
+    // Define click listener on bottom navigation
+    private BottomNavigationView.OnItemSelectedListener navItemSelectedListener = new BottomNavigationView.OnItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                default:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.nav_shop:
+                    fragment = new ShopFragment();
+                    break;
+                case R.id.nav_category:
+                    fragment = new CategoryFragment();
+                    break;
+                case R.id.nav_search:
+                    fragment = new SearchFragment();
+                    break;
+                case R.id.nav_profile:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+    };
+
+    private void queryItem() {
+        // Define the class we would like to query
+        ParseQuery<Item> itemParseQuery = ParseQuery.getQuery(Item.class);
+
+        // Include user key
+        itemParseQuery.include(Item.KEY_ITEM_CATEGORY);
+        // Execute the find asynchronously
+        itemParseQuery.findInBackground(new FindCallback<Item>() {
+            @Override
+            public void done(List<Item> items, ParseException e) {
+                if (e != null){
+                    Log.e("HomeActivity", "ERROR while retrieving posts", e);
+                    return;
+                }
+
+                for (Item item: items){
+                    Log.i("HomeActivity", "Item: " + item.getItemName() + " Category Name: " + item.getCategory().getCategoryName());
+                }
+            }
+        });
+    }
+}
