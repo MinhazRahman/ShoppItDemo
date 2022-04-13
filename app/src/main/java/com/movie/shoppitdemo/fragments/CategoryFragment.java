bf58f8ce00dba1fefecd96d6aebbe1ca4a4d6791
Mohammed Rahman <minhaz357@gmail.com>
2022-04-13 00:06:24 -0400

    Retrieve all the categories from Parse backend

--- app/src/main/java/com/movie/shoppitdemo/fragments/CategoryFragment.java
+++ app/src/main/java/com/movie/shoppitdemo/fragments/CategoryFragment.java
@@ -2,15 +2,30 @@ package com.movie.shoppitdemo.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.movie.shoppitdemo.R;
+import com.movie.shoppitdemo.models.Category;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
+    public static final String TAG = "CategoryFragment";
+
+    RecyclerView rvCategories;
+    //CategoryAdapter categoryAdapter;
+    List<Category> allCategories;
 
     public CategoryFragment() {
         // Required empty public constructor
@@ -26,5 +41,48 @@ public class CategoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Setup any handles to view objects here
+        rvCategories = view.findViewById(R.id.rvCategories);
+
+        // Initialize the list
+        allCategories = new ArrayList<>();
+
+        // Get all the categories
+        getAllCategories();
+    }
+
+    // Retrieve all the categories from the Parse backend
+    private void getAllCategories() {
+        // Define the class we would like to query
+        ParseQuery<Category> categoryParseQuery = ParseQuery.getQuery(Category.class);
+
+        // Include user key
+        categoryParseQuery.include(Category.KEY_CATEGORY_NAME);
+        // Execute the find asynchronously
+        categoryParseQuery.findInBackground(new FindCallback<Category>() {
+            @Override
+            public void done(List<Category> categories, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "ERROR while retrieving categories", e);
+                    return;
+                }
+
+                for (Category category: categories){
+                    Log.i(TAG,  " Category Name: " + category.getCategoryName());
+                }
+
+                // Add all categories to the list
+                allCategories.addAll(categories);
+                // Notify the adapter about the data change
+            }
+        });
     }
 }
--- app/src/main/java/com/movie/shoppitdemo/models/Category.java
+++ app/src/main/java/com/movie/shoppitdemo/models/Category.java
@@ -1,6 +1,7 @@
 package com.movie.shoppitdemo.models;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 
 import org.parceler.Parcel;
@@ -9,6 +10,7 @@ import org.parceler.Parcel;
 @Parcel(analyze={Category.class})
 public class Category extends ParseObject {
     public static final String KEY_CATEGORY_NAME = "categoryName";
+    public static final String KEY_CATEGORY_IMAGE = "categoryImage";
 
     // empty constructor needed by the Parceler library
     public Category(){
@@ -20,5 +22,13 @@ public class Category extends ParseObject {
 
     public void setCategoryName(String itemName){
         put(KEY_CATEGORY_NAME, itemName);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_CATEGORY_IMAGE);
+    }
+
+    public void setImage(ParseFile parseFile){
+        put(KEY_CATEGORY_IMAGE, parseFile);
     }
 }
